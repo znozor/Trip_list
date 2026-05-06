da388a7ea6d07c4195777856675dfad21099d665
znozor <iamshakil658@gmail.com>
2026-05-06 17:31:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 const CONFIG = {
   SUPABASE_URL: 'https://ezmtdoodmkszngfndkpg.supabase.co',
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6bXRkb29kbWtzem5nZm5ka3BnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyMjg1ODIsImV4cCI6MjA5MjgwNDU4Mn0.aWgag4LTNhU-eDFyhHKlADNi0ERwbB45eScxvLwhjiE',
-  OPENWEATHER_API_KEY: '', // optional
+  WEATHERBIT_API_KEY: '4605b539192d48138ef19b2f4c5057fe', // optional
   AMAZON_ASSOCIATE_TAG: 'your-tag-20'         // optional
 };
