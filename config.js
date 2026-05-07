e4843a5ba78e14abb770ae76bc94973f994b3980
znozor <iamshakil658@gmail.com>
2026-05-07 20:40:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js – REPLACE with your Supabase project URL and anon key
-const CONFIG = {
+window.CONFIG = {
   SUPABASE_URL: 'https://ezmtdoodmkszngfndkpg.supabase.co',
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6bXRkb29kbWtzem5nZm5ka3BnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyMjg1ODIsImV4cCI6MjA5MjgwNDU4Mn0.aWgag4LTNhU-eDFyhHKlADNi0ERwbB45eScxvLwhjiE',
   WEATHERBIT_API_KEY: '4605b539192d48138ef19b2f4c5057fe', // optional
